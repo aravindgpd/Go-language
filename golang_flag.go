46421aa6a2f49876e01381232d9f482622134c6d
aravindgpd <aravindgpd@gmail.com>
2019-10-26 08:20:25 +0530

    adding the example for golang flag

--- golang_flag.go
+++ golang_flag.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	flag
-	fmt
+	"flag"
+	"fmt"
 )
 
 func main() {
 
-	wordPtr := flag.String("word"," foo ","a string")
-	numbPtr := flag.Int("numb",42,"an int")
-	boolPtr := flag.Bool("fork",false,"a bool")
+	wordPtr := flag.String("word", " foo ", "a string")
+	numbPtr := flag.Int("numb", 42, "an int")
+	boolPtr := flag.Bool("fork", false, "a bool")
 
 	var svar string
-	flag.StringVar(&svar,"svar","bar","a string var")
+	flag.StringVar(&svar, "svar", "bar", "a string var")
 
 	flag.Parse()
 
